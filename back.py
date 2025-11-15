e5a800399eff5968940b4cbc67343e99229fcc9b
Stepan Komarov <@dendi39>
2025-11-15 19:58:51 +0300

    15.11.25 commit

--- back.py
+++ back.py
@@ -1,54 +1,76 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
+from typing import Annotated
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
 app = FastAPI()
+engine = create_async_engine('sqlite+aiosqlite:///database.db')
+
+new_session = async_sessionmaker(engine, expire_on_commit=False)
+
+async def get_session():
+    async with new_session() as session:
+        yield session
+
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
+
+class Base(DeclarativeBase):
+    pass
+
+class PlaceModel(Base):
+    __tablename__ = "places"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    adding_data: Mapped[str]
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-places = [
-    {
-    "id":1,
-    "name":"Red Square",
-    "adding data":"31.10.25"
-    },
-    {
-    "id":2,
-    "name":"Petrovkyy Park",
-    "adding data":"31.10.25"
-    },
-    {
-    "id":3,
-    "name":"1231412",
-    "adding data":"04.11.1941"
-    },
-]
+@app.post('/setup_database')
+async def setup_database():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+    return {"ok": True}
 
-@app.get("/places",
-         tags=["Места"],
-         summary="Получить все места")
-def place():
-    return places
+class PlaceAddSchema(BaseModel):
+    name: str
+    adding_data: str
 
-@app.get(
-    "/places/{place_id}",
-         tags=["Места"],
-         summary="Получить конкретное место")
-def get_book(place_id: int):
-    for place in places:
-        if place["id"] == place_id:
-            return place
-    raise HTTPException(status_code=404, detail="Место не найдено")
+class PlaceSchema(PlaceAddSchema):
+    id: int
+
+
+@app.get('/places')
+async def get_places(session: SessionDep):
+    query = select(PlaceModel)
+    result = await session.execute(query)
+    return result.scalars().all()
+
+
+# @app.get(
+#     "/places/{place_id}",
+#          tags=["Места"],
+#          summary="Получить конкретное место")
+# def get_book(place_id: int):
+#     for place in places:
+#         if place["id"] == place_id:
+#             return place
+#     raise HTTPException(status_code=404, detail="Место не найдено")
 
 class NewPlace(BaseModel):
     name: str
     adding_data: str
 
-@app.post("/places", tags=["Места"])
-def create_book(new_place: NewPlace):
-    places.append({
-        "id": len(places) + 1,
-        "title": new_place.name,
-        "author": new_place.adding_data,
-    })
-    return {"success": True}
+@app.post('/places')
+async def add_places(data: PlaceAddSchema, session: SessionDep):
+    new_place = PlaceModel(
+        name=data.name,
+        adding_data=data.adding_data
+    )
+    session.add(new_place)
+    await session.commit()
+    return {"ok": True}
